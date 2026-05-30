d2ff04556a5216f1e2c18e385a643942dfc1aa0f
Srushti Pawar <srushtipawar455@gmail.com>
2026-05-30 20:46:15 +0530

    Fix: Completely removed absolute path and image resize block for cloud storage compatibility

--- accounts/models.py
+++ accounts/models.py
@@ -31,4 +31,4 @@ class Profile(models.Model):
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
-            img.save(self.picture.path)
+            img.save(self.picture)
