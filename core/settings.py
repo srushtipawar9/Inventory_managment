f2eabb27c278090520abda844ac052ab5932be94
Srushti Pawar <srushtipawar455@gmail.com>
2026-06-03 14:20:15 +0530

    added

--- core/settings.py
+++ core/settings.py
@@ -109,9 +109,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
 
-    
+    'django.contrib.staticfiles',
     'cloudinary_storage',
-    'django.contrib.staticfiles',  
     'cloudinary',
 
     
